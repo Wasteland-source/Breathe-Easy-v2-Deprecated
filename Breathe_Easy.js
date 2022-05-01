ea472404672207e1cf4cb830d94b77074d7f818f
Wasteland-source <54219063+Wasteland-source@users.noreply.github.com>
2022-05-02 00:23:02 +0100

    Update Breathe_Easy.js

--- Breathe_Easy.js
+++ Breathe_Easy.js
@@ -25,9 +25,12 @@ const admin = require('./Commands/BreatheEasy_Admin.js'),
 const adminID = ('343352390977585162');
 //Seting myself as admin so i can limit some commands
 
+botBE.login(auth.token);
+//Logs into the client using the bot token
+
 botBE.on('ready', function() {
   console.log('\x1b[36m%s\x1b[0m', `Breathe Easy has successfully launched`);
-  console.log('\x1b[36m%s\x1b[0m', `Breathe Easy has connected to ${botBE.guilds.size} guilds with ${botBE.guilds.reduce((uCount, guild)=>uCount+guild.memberCount, 0)} users`);
+  //console.log('\x1b[36m%s\x1b[0m', `Breathe Easy has connected to ${botBE.guilds.size} guilds with ${botBE.guilds.reduce((uCount, guild)=>uCount+guild.memberCount, 0)} users`);
   botBE.user.setActivity('/help for help || Everything is going to be okay, I promise <3');
 });
 //Logging messages to console when Breathe Easy comes online
@@ -51,12 +54,16 @@ botBE.on('message', async function(msg) {
     case 'help': //Sends a help block
       help(msg.author.tag, msg.guild, msg.channel);
       break;
+    case 'invite': //Sends an invite link
+      msg.channel.send('Feel free to invite me to your server, I\'ll do the very best!:\nhttps://discordapp.com/api/oauth2/authorize?client_id=626274863220523008&permissions=0&scope=bot');
+      break;
     case 'feedback': //Sends feedback to me via the bot
-      feedback(msg.content.slice(command.length + 1), msg.author.tag, msg.guild, msg.channel, botBE.guilds.get('560014471599489046').channels.get('627248802084159507'));
+      msg.channel.send('request and feedback are proving problematic and will unfortunately be down until I am able to fix them, feel free to message me personally here if you need anything: Wasteland Doctrine#9303');
+      //feedback(msg.content.slice(command.length + 1), msg.author.tag, msg.guild, msg.channel, botBE.guilds.fetch('560014471599489046').channels.fetch('627248802084159507'));
       break;
     case 'admin': //Admin commands
       if (msg.author.id === adminID) {
-        admin(args[0], botBE, auth.token, msg.author.tag, msg.guild, botBE.guilds.size, botBE.guilds.map(guild => guild.name).join("\n"), botBE.guilds.map(guild => guild.id).join("\n"), botBE.guilds.reduce((uCount, guild)=>uCount+guild.memberCount, 0), msg.content.slice(command.length + 9), msg.channel);
+        admin(args[0], botBE, auth.token, msg.author.tag, msg.guild, botBE.guilds.size, botBE.guilds.map(guild => guild.name).join("\n"), botBE.guilds.map(guild => guild.id).join("\n"), botBE.guilds.reduce((uCount, guild) => uCount + guild.memberCount, 0), msg.content.slice(command.length + 9), msg.channel);
       } else {
         msg.channel.send('ONLY DAD CAN TELL ME WHAT TO DO');
         console.log('\x1b[35m%s\x1b[0m', `${msg.guild}:${msg.author.tag} Admin command refuse\n`);
@@ -66,7 +73,8 @@ botBE.on('message', async function(msg) {
       prompt(args[0], msg.author.tag, msg.guild, msg.channel);
       break;
     case 'request': //Allows users to send requests for prompts to me
-      request(msg.content.slice(command.length + 1), msg.author.tag, msg.guild, msg.channel, botBE.guilds.get('560014471599489046').channels.get('627342241253425153'));
+      msg.channel.send('request and feedback are proving problematic and will unfortunately be down until I am able to fix them, feel free to message me personally here if you need anything: Wasteland Doctrine#9303');
+      //request(msg.content.slice(command.length + 1), msg.author.tag, msg.guild, msg.channel, botBE.guilds.fetch('560014471599489046').channels.fetch('627342241253425153'));
       break;
     case 'breath': //Sends a breathing exercise
     case 'breathe':
@@ -76,7 +84,7 @@ botBE.on('message', async function(msg) {
       count(msg.author.tag, msg.guild, msg.channel);
       break;
     case 'hug': //Sends a hug to people you mention
-      hug(msg.mentions.members.first(), msg.author.tag, msg.guild, msg.author.username, msg.channel, botBE.user.id);
+      hug(msg.mentions.users.first(), msg.author.tag, msg.guild, msg.author.username, msg.channel, botBE.user.id);
       break;
     case 'happy': //Sends encouraging messages
       encourage(msg.author.tag, msg.guild, msg.channel);
@@ -99,6 +107,3 @@ botBE.on('guildCreate', async function(guild) {
 botBE.on('guildDelete', function(guild) {
   console.log('\x1b[36m%s\x1b[0m', `Breathe Easy has left ${guild.name}`);
 }); //logging guild leaves
-
-botBE.login(auth.token);
-//Logs into the client using the bot token
